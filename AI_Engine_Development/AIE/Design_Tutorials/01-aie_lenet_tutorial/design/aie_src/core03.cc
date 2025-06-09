b145bf92a890893f51f36a33a398552e6630351d
Nirmal Senthilkumar <nirmal.senthilkumar@amd.com>
2025-06-08 21:55:04 -0700

    Update core03.cc

--- AI_Engine_Development/AIE/Design_Tutorials/01-aie_lenet_tutorial/design/aie_src/core03.cc
+++ AI_Engine_Development/AIE/Design_Tutorials/01-aie_lenet_tutorial/design/aie_src/core03.cc
@@ -3,14 +3,21 @@ Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 SPDX-License-Identifier: MIT
 */
 
+
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "core03.h"
-#include "core03lut.h"
+#include "core05.h"
+#include "core05lut.h"
+
+
+
+
+
+
 
 #ifndef INLINE
-INLINE_DECL void core03(
+INLINE_DECL void core05(
         const int RowA_tile,
         const int ColA_tile,
         const int ColB_tile,
@@ -34,11 +41,12 @@ INLINE_DECL void core03(
 	//********** Mul Intrinsic********/
 
 	using MMUL = aie::mmul<4, 8, 4, int8_t, int8_t>;
+
 	
 
-        unsigned int i,j,z;
+    	int i,j,z;
 	
-	for (z=0; z<RowA_tile/2; z++) chess_loop_range(2,)
+	for (z=0; z<RowA_tile; z++) chess_loop_range(2,)
 	{
 
 		//********** Output vector ********/
@@ -50,8 +58,8 @@ INLINE_DECL void core03(
 		{		   	  	
 			 const int8_t * __restrict pA1 = A_in + (      z * ColA_tile +       0) * sizeTileA;
           		 const int8_t * __restrict pA2 = A_in + ((z + 1) * ColA_tile +       0) * sizeTileA;
-          		 const int8_t * __restrict pB1 = B03 + (      0 * ColB_tile +       j) * sizeTileB;
-          		 const int8_t * __restrict pB2 = B03 + (      0 * ColB_tile + (j + 1)) * sizeTileB;
+          		 const int8_t * __restrict pB1 = B05 + (      0 * ColB_tile +       j) * sizeTileB;
+          		 const int8_t * __restrict pB2 = B05 + (      0 * ColB_tile + (j + 1)) * sizeTileB;
 
  
 
@@ -64,14 +72,15 @@ INLINE_DECL void core03(
           		 aie::vector<int8_t, sizeTileB> B1 = aie::load_v<sizeTileB>(pB2); pB2 += sizeTileB * ColB_tile;
 
 
+
+
           		 MMUL C00; C00.mul(A0, B0);
           		 MMUL C01; C01.mul(A0, B1);
           		 MMUL C10; C10.mul(A1, B0);
           		 MMUL C11; C11.mul(A1, B1);
 
           		for (i = 1; i < ColA_tile; ++i) chess_prepare_for_pipelining chess_loop_range(3,) {
-         
-	 		     A0 = aie::load_v<sizeTileA>(pA1); pA1 += sizeTileA;
+          		     A0 = aie::load_v<sizeTileA>(pA1); pA1 += sizeTileA;
           		     A1 = aie::load_v<sizeTileA>(pA2); pA2 += sizeTileA;
           		     B0 = aie::load_v<sizeTileB>(pB1); pB1 += sizeTileB * ColB_tile;
           		     B1 = aie::load_v<sizeTileB>(pB2); pB2 += sizeTileB * ColB_tile;
@@ -80,55 +89,80 @@ INLINE_DECL void core03(
           		     C10.mac(A1, B0);
           		     C11.mac(A1, B1);
           		 }
-			 //*********will require rearrangement of output**********//
-			 //*************bsrs and shift****************************//
-			 
+			 //**************will require rearrangement of output*****************//
+			 //*********************bsrs and shift********************************//
+
           		 aie::store_v(pC1, C00.template to_vector<int8_t>(shift)); pC1 += sizeTileC;
           		 aie::store_v(pC1, C01.template to_vector<int8_t>(shift)); pC1 += sizeTileC;
           		 aie::store_v(pC2, C10.template to_vector<int8_t>(shift)); pC2 += sizeTileC;
           		 aie::store_v(pC2, C11.template to_vector<int8_t>(shift)); pC2 += sizeTileC;
 
-
-
-
 		}
 	
 	}
 
+
     int8_t* ptrC_relu = C_out;
-    int num_iterations = COL_B_3/tileX_C;
+    int num_iterations = (COL_B_5/tileX_C) - 2;
     int r,c;
 
-    for(r=0;r<num_iterations;r++) {
-      for(c=0;c<tileX_C;c++) {
-        if (*ptrC_relu < 0) {
-          *ptrC_relu = 0;
-        }
-        ptrC_relu++;
-      }
-      ptrC_relu += tileX_C * (tileY_C - 1);
-    }
+	/* optimized ReLu with vectors */
 
+    constexpr int VECTOR_SIZE = 16;
+	int VECTORS_PER_ROW = tileX_C / VECTOR_SIZE;
+	int REMAINDER = tileX_C % VECTOR_SIZE;
 
+	for(r = 0; r < num_iterations; r++) {
+		int8_t* row_ptr = ptrC_relu;
+		
+		// Process full vectors
+		for(c = 0; c < VECTORS_PER_ROW; c++) {
+			// Load 32 int8 values into vector
+			aie::vector<int8_t, VECTOR_SIZE> data_vec = aie::load_v<VECTOR_SIZE>(row_ptr);
+			
+			// Create zero vector for comparison
+			aie::vector<int8_t, VECTOR_SIZE> zero_vec = aie::zeros<int8_t, VECTOR_SIZE>();
+			
+			// Apply ReLU: max(data, 0)
+			aie::vector<int8_t, VECTOR_SIZE> relu_vec = aie::max(data_vec, zero_vec);
+			
+			// Store result back
+			aie::store_v(row_ptr, relu_vec);
+			
+			row_ptr += VECTOR_SIZE;
+		}
+		
+		// Handle remainder elements (if tileX_C is not divisible by VECTOR_SIZE)
+		if(REMAINDER > 0) {
+			for(int i = 0; i < REMAINDER; i++) {
+				if(*row_ptr < 0) {
+					*row_ptr = 0;
+				}
+				row_ptr++;
+			}
+		}
+		
+		// Move to next row with stride
+		ptrC_relu += tileX_C * tileY_C;
+	}
+    *ptrC_relu = 8;
 }
 #endif
 
-void core03_top(input_buffer<int> & __restrict inA, output_buffer<int> & __restrict out){
-	int shift = 6;
-    set_sat();
-    set_rnd(rnd_sym_inf);
+void core05_top(input_buffer<int> & __restrict inA, output_buffer<int> & __restrict out){
+  int shift = 6;
+  set_sat();
+  set_rnd(rnd_sym_inf);
+  int tileX_A = 8;
+  int tileY_A = 4;
+  int tileX_C = 4;
+  int tileY_C = 4;
 
-	int tileX_A = 8;
-	int tileY_A = 4;
-	int tileX_C = 4;
-	int tileY_C = 4;
-	
-	int *__restrict ptr = inA.data();
-	int *__restrict ptr_1 = out.data();
+  int *__restrict ptr = inA.data();
+  int *__restrict ptr_1 = out.data();
 
-	core03(ROW_A_3 >> 2, COL_A_3 >> 3, COL_B_3 >> 2, 
-           tileX_A, tileY_A, tileX_C, tileY_C, 
-           (int8_t *)ptr, (int8_t *)ptr_1, 
-           shift);
+  core05(ROW_A_5 >> 2, COL_A_5 >> 3, COL_B_5 >> 2, 
+         tileX_A, tileY_A, tileX_C, tileY_C, 
+        (int8_t *)ptr, (int8_t *)ptr_1, 
+         shift);
 }
-
